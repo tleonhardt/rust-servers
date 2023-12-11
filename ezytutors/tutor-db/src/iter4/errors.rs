7cf0cfc4d96dc5b4d1886ee16311b81127e9b607
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-12-10 22:16:02 -0500

    Started implementing custom error type enum

---
+++ ezytutors/tutor-db/src/iter4/errors.rs
@@ -0,0 +1,15 @@
+use actix_web::{error, http::StatusCode, HttpResponse, Result};
+use serde::Serialize;
+use sqlx::error::Error as SQLxError;
+use std::fmt;
+
+#[derive(Debug, Serialize)]
+pub enum EzyTutorError {
+    DBError(String),
+    ActixError(String),
+    NotFound(String),
+}
+#[derive(Debug, Serialize)]
+pub struct MyErrorResponse {
+    error_message: String,
+}
