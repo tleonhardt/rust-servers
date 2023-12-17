556d948457c2d5fe83ee37917a3322cf1a0b71b3
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-12-17 10:31:42 -0500

    Added custom error type

--- ezytutors/tutor-db/src/iter4/errors.rs
+++ ezytutors/tutor-db/src/iter4/errors.rs
@@ -9,7 +9,44 @@ pub enum EzyTutorError {
     ActixError(String),
     NotFound(String),
 }
+
 #[derive(Debug, Serialize)]
 pub struct MyErrorResponse {
     error_message: String,
+}
+
+impl EzyTutorError {
+    fn error_response(&self) -> String {
+        match self {
+            EzyTutorError::DBError(msg) => {
+                println!("Database error occurred: {:?}", msg);
+                "Database error".into()
+            }
+            EzyTutorError::ActixError(msg) => {
+                println!("Server error occurred: {:?}", msg);
+                "Internal server error".into()
+            }
+            EzyTutorError::NotFound(msg) => {
+                println!("Not found error occurred: {:?}", msg);
+                msg.into()
+            }
+        }
+    }
+}
+
+impl error::ResponseError for EzyTutorError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            EzyTutorError::DBError(msg) | EzyTutorError::ActixError(msg) => {
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+            EzyTutorError::NotFound(msg) => StatusCode::NOT_FOUND,
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code()).json(MyErrorResponse {
+            error_message: self.error_response(),
+        })
+    }
 }
