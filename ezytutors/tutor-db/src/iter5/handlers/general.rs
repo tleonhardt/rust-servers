b8b7609ccf2a0126e6ff6e88b06587d4d24f6fe4
Todd Leonhardt <todd.leonhardt@gmail.com>
2024-01-02 12:16:26 -0500

    Added health check unit test

--- ezytutors/tutor-db/src/iter5/handlers/general.rs
+++ ezytutors/tutor-db/src/iter5/handlers/general.rs
@@ -8,3 +8,27 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
     *visit_count += 1;
     HttpResponse::Ok().json(&response)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::StatusCode;
+    use dotenv::dotenv;
+    use sqlx::postgres::PgPool;
+    use std::env;
+    use std::sync::Mutex;
+
+    #[actix_rt::test]
+    async fn health_check_success() {
+        dotenv().ok();
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+        let pool: PgPool = PgPool::connect(&database_url).await.unwrap();
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: pool,
+        });
+        let resp = health_check_handler(app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+}
